fa9097947d8f7106767b092b92b2d7cbe411793b
Neil de Carteret <ndecarteret@sportingindex.com>
2017-10-23 14:50:06 +0100

    initial commit

---
+++ code.js
@@ -0,0 +1,29 @@
+const foo = bar => baz => {
+	return doAVeryLongThingBlahBlahBlahWibble().then(response => {
+		
+	}
+																									)
+}
+
+
+
+
+doAVeryLongThingBlahBlahBlahWibble().then(response => {
+	// whatever
+})
+
+
+doAVeryLongThingBlahBlah().then(response => {
+	// whatever
+},
+																/* whut */)
+
+doAVeryLongThingBlahBlah().then(response => {
+	// handle success
+},
+error => {
+	// deal with error
+})
+
+
+
